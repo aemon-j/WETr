abdfd6c5ecbe797250aca88ee631f9279eae4156
robertladwig <ladwigjena@gmail.com>
2026-06-22 14:47:07 +0200

    compiled ubuntu version for lerwq

--- R/run_wet.R
+++ R/run_wet.R
@@ -84,7 +84,7 @@ run_wetWin <- function(sim_folder, yaml_file = 'gotm.yaml',
 run_wetNIX <- function(sim_folder, verbose=TRUE, args){
   origin <- getwd()
   setwd(sim_folder)
-  wet_path <- system.file('exec/nixgotm', package=packageName())
+  wet_path <- system.file('exec/gotm', package=packageName())
   Sys.setenv(LD_LIBRARY_PATH=paste(system.file('extbin/nix',
                                                package=packageName()),
                                    Sys.getenv('LD_LIBRARY_PATH'),
